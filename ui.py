d112a0bbaaadda4d897395f5df0f5c6a5cc81088
Pablos <pablos@list.ru>
2022-02-14 21:35:30 +0300

    import in dict & csv export modules added

--- ui.py
+++ ui.py
@@ -1,19 +1,37 @@
 
 def import_file():
     with open('spravochnik.txt', encoding='UTF8') as file:
-        a = file.read()
-        return a
+        return file.read()
 
-def save_txt(file):
-    file1 = import_file()
-    with open('new_spravochnik.txt', 'w', encoding='UTF8') as file:
-        file.write(file1)
+
+def import_file_2_dict(adr):
+    with open(adr, encoding='UTF8') as file:
+        tmp = [line.strip() for line in file.readlines() if line != '\n']
+        pb = {}
+        for i in range(0, len(tmp), 4):
+            pb.update({tmp[i+2]: [tmp[i], tmp[i+1], tmp[i+3]]})
+    return pb
+
+def d_item_2_csv(v):
+    st = ''
+    for i in range(len(v)):
+        st += ',' + v[i]
+    return st
+
+def save_2_csv(f):
+    export_to = 'new_spravochnik.txt'
+    with open(export_to, 'w', encoding='UTF8') as file:
+        for k,v in f.items():
+            file.write(f'{k}{d_item_2_csv(v)}\n')
+    print(f"Файл {export_to} сохранён.")
+
 
 def inp():
     print('Операции со справочником: \n1 - вывести в консоль\n2 - сохранить в файл txt')
     return input('Введите команду: ')
 
-def output(file):
-    print(file)
 
-
+def output(f):
+    print()
+    for k,v in f.items():
+            print(f'{k} : {v}\n')
